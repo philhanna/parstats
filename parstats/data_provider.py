5c876d2528908d7dbb2451c54a5e5d0955638ce3
Phil Hanna <ph1204@gmail.com>
2023-08-09 22:15:53 -0400

    Finished constructor

--- parstats/data_provider.py
+++ parstats/data_provider.py
@@ -1,5 +1,5 @@
 import configparser
-import os.path
+import os
 
 
 class DataProvider:
@@ -7,14 +7,31 @@ class DataProvider:
     obtained from the .config/gnome-games/aisleriot file
     """
     def __init__(self, filename=None):
+
+        # If the user specified a configuration file name, use that.
+        # Otherwise, use the default file name, which is
+        # ~/.config/gnome-games/aisleriot
+
         if not filename:
             filename = DataProvider.default_filename()
+        
+        # Make sure it exists
+        if not os.path.exists(filename):
+            raise FileNotFoundError(f"{filename} does not exist")
+        
+        # Create a ConfigParser object and read the .ini file into it
+        
+        self.config = configparser.ConfigParser()
+        self.config.read(filename)
+        
 
     @staticmethod
     def default_filename() -> str:
         """Returns the name of the .ini file containing aisleriot
-        configuration and statistics
+        configuration and statistics.  Note: in this case, the file
+        name does not actually end in ".ini", but its contents are
+        in that format.
         """
         config_dir = os.path.expanduser("~/.config")
-        filename = os.path.join(config_dir, "gnome-games", "aisleriot.ini")
+        filename = os.path.join(config_dir, "gnome-games", "aisleriot")
         return filename
