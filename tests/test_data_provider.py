463ffa1382d7df11b47a67ead2da83c6a1092cd4
Phil Hanna <ph1204@gmail.com>
2023-08-09 22:41:32 -0400

    Added unit test

--- tests/test_data_provider.py
+++ tests/test_data_provider.py
@@ -5,3 +5,7 @@ from parstats import DataProvider
 def test_constructor_with_bogus_filename():
     with pytest.raises(FileNotFoundError):
         DataProvider("Bogus")
+
+def test_constructor_with_default_filename():
+    dp = DataProvider()
+    assert dp is not None
