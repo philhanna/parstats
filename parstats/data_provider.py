635cd327be10f41864e908374eac3895282fbe09
Phil Hanna <ph1204@gmail.com>
2023-08-09 22:37:39 -0400

    Fixed type hint syntax

--- parstats/data_provider.py
+++ parstats/data_provider.py
@@ -20,16 +20,15 @@ class DataProvider:
 
         if not filename:
             filename = DataProvider.get_default_filename()
-        
+
         # Make sure it exists
         if not os.path.exists(filename):
             raise FileNotFoundError(f"{filename} does not exist")
-        
+
         # Create a ConfigParser object and read the .ini file into it
-        
+
         self.config = configparser.ConfigParser()
         self.config.read(filename)
-        
 
     @staticmethod
     def get_default_filename() -> str:
@@ -39,20 +38,19 @@ class DataProvider:
         name does not actually end in ".ini", but its contents are
         in that format.
         """
-        config_dir = os.path.expanduser("~/.config")
-        filename = os.path.join(config_dir, "gnome-games", "aisleriot")
+        config_dir: str = os.path.expanduser("~/.config")
+        filename: str = os.path.join(config_dir, "gnome-games", "aisleriot")
         return filename
-    
 
-    def get_game_list(self) -> list(str) | None:
+    def get_game_list(self) -> list[str] | None:
         """
         Returns the list of all games played so far, based on the
         "Recent" list in the header section. If no games have been played,
         returns None.
         """
-        item = self.Sections[self.HEADER_SECTION][self.RECENT_ITEM]
+        item: str = self.Sections[self.HEADER_SECTION][self.RECENT_ITEM]
         if not item:
             return None
         item = item.rstrip(";")
-        lst = item.split(";")
+        lst: list[str] = item.split(";")
         return lst
