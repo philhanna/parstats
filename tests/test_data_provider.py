8a9af515be3d575a92dc19eb9d607f19a43b013c
Phil Hanna <ph1204@gmail.com>
2023-08-09 22:47:04 -0400

    Added pytest fixture for data provider

--- tests/test_data_provider.py
+++ tests/test_data_provider.py
@@ -1,11 +1,21 @@
+import getpass
 import pytest
 from parstats import DataProvider
 
+@pytest.fixture
+def dp():
+    return DataProvider()
 
 def test_constructor_with_bogus_filename():
     with pytest.raises(FileNotFoundError):
         DataProvider("Bogus")
 
-def test_constructor_with_default_filename():
-    dp = DataProvider()
+def test_constructor_with_default_filename(dp):
     assert dp is not None
+
+def test_get_default_filename(dp):
+    dp = DataProvider()
+    username = getpass.getuser()
+    filename = dp.get_default_filename()
+    assert filename is not None
+    assert username in filename
