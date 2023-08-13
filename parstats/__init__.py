c759b869921e80dc53e823fed04d94f118b5d2a5
Phil Hanna <ph1204@gmail.com>
2023-08-13 17:12:49 -0400

    Added .ini section or item keys

--- parstats/__init__.py
+++ parstats/__init__.py
@@ -1,5 +1,12 @@
+HEADER_SECTION = "Aisleriot Config"
+RECENT_ITEM = "recent"
+STATS_KEY = "statistic"
+
 from .data_provider import DataProvider
 
 __all__ = [
+    'HEADER_SECTION',
+    'RECENT_ITEM',
+    'STATS_KEY',
     'DataProvider',
 ]
