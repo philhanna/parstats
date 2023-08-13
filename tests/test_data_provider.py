35c2ed8b1a81026e76c5238c188a7ddc45769df8
Phil Hanna <ph1204@gmail.com>
2023-08-13 17:23:46 -0400

    Worked on test for get_game_list method

--- tests/test_data_provider.py
+++ tests/test_data_provider.py
@@ -1,9 +1,11 @@
+import configparser
 import getpass
 import os.path
 
 import pytest
 
 from parstats import DataProvider
+from parstats import HEADER_SECTION, RECENT_ITEM
 
 
 @pytest.fixture
@@ -31,17 +33,18 @@ def test_get_default_filename(dp):
     assert username in filename
 
 @pytest.mark.parametrize("filename, expected, expected_error", [
-    (os.path.join("testdata", "aisleriot"), ["spider", "freecell", "canfield", "klondike"], False),
-    (os.path.join("testdata", "stooges.ini"), None, False),
-    (os.path.join("testdata", "non-existent.ini"), None, True),
-    (os.path.join("testdata", "bogus.ini"), None, True),
-    (os.path.join("testdata", "bogus2.ini"), None, True),
+    (os.path.join("testdata", "aisleriot"), ["spider", "freecell", "canfield", "klondike"], None),
+    (os.path.join("testdata", "stooges.ini"), None, None),
+    #(os.path.join("testdata", "non-existent.ini"), None, FileNotFoundError),
+    #(os.path.join("testdata", "bogus.ini"), None, configparser.Error),
+    #(os.path.join("testdata", "bogus2.ini"), None, configparser.Error),
 ])
 def test_data_provider_game_list(filename, expected, expected_error):
     if expected_error:
-        with pytest.raises(FileNotFoundError):
+        with pytest.raises(expected_error):
             DataProvider(filename)
     else:
         dp = DataProvider(filename)
         actual = dp.get_game_list()
-        assert set(actual) == set(expected)
+        if actual:
+            assert set(actual) == set(expected)
