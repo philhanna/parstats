ad04d09ef10c4f1027453fc03659bb0b1ed691fc
Phil Hanna <ph1204@gmail.com>
2023-08-13 20:50:05 -0400

    Added setup.py

---
+++ setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup
+
+setup(
+    name='Aisleriot Statistics',
+    version='1.0.0',
+    packages=['arstats'],
+    url='https://github.com/philhanna/parstats',
+    license='MIT',
+    author='Phil Hanna',
+    author_email='ph1204@gmail.com',
+    description='Displays statistics for Linux Aisleriot card games',
+    install_requires=[
+        'pytest',
+    ],
+)
